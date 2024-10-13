b94d74af9925cc27e2d8d8aef2a15892c3c7ddf7
SundousKanaan <94317411+SundousKanaan@users.noreply.github.com>
2024-10-13 18:07:14 +0100

    update: nav button css

---
+++ javascript/javascript.js
@@ -0,0 +1,6 @@
+let navButton = document.querySelector(".navButton");
+let nav = document.querySelector("header nav");
+
+navButton.addEventListener("click", function () {
+  navButton.classList.toggle("open");
+});
